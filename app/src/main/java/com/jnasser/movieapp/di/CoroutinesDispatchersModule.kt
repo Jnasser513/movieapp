d87300fa7e2f0483c0dd9ffad957540b51eaecca
jnasser3 <josenasser@sdsalgroup.com>
2024-03-14 10:50:28 -0600

    Agregando modulo para inyectar Dispatchers

---
+++ app/src/main/java/com/jnasser/movieapp/di/CoroutinesDispatchersModule.kt
@@ -0,0 +1,43 @@
+package com.jnasser.movieapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutinesDispatchersModule {
+
+    @IoDispatcher
+    @Provides
+    @Singleton
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    @Singleton
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @DefaultDispatcher
+    @Provides
+    @Singleton
+    fun provideSingletonDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+}
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultDispatcher
